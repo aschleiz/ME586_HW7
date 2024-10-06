0c52e24857c5b5639ef31909d31aefa8d3760cbe
aschleiz <aschleiz@purdue.edu>
2024-10-06 19:01:54 -0400

    First commit, added empty project file for P2 for now

---
+++ Downloads/HW6_P1/main.c
@@ -0,0 +1,89 @@
+/**
+  ******************************************************************************
+  * @file    main.c 
+  * @author  Zhou Zeng
+  * @version V1.1.0
+  * @date    10-Oct-2018
+  * @brief   Main program body.
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "ME586.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+int outval = 0;
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+void checkloop(void);
+int readloop(void);
+void do_nothing(int);
+
+int main(void)
+{
+	  initcom();
+		printf("Press a key to start the process! ");
+		WaitForKeypress();
+		putchar('\n');
+		putchar('\r');
+		while(1){
+			//checkloop();
+			outval = readloop();
+	//		shownum(outval);
+	//		putchar('\n');
+	//		putchar('\r');
+		};
+} //end of main program
+
+
+void checkloop(void){
+	int tmp = 0x00;
+	while (tmp == 0x00){
+		checkcom();
+		
+	}
+}
+
+int readloop(void){
+	int count = 0;
+	short digval = 0;
+	initports(0x100);
+	while(1){
+		digval = digin();
+		//digval >> 8;
+		digval = digval & 0x0100; // Mask unnecessary bits, might not be needed
+		if (digval != 0x00){
+			count++;
+			//initports(0x0);
+			digout(count);
+			do_nothing(0x000FFFFF);
+		}
+		else{
+			if(count != 0){
+				shownum(count);
+				putchar('\n');
+				putchar('\r');
+			break;
+			}
+		}
+	}
+	return count;
+}
+
+void do_nothing(int len){
+	int counter = 0;
+	while(counter < len){
+		counter++;
+	}
+}
+
+void timehand(void){
+}
+
+void inthand(void){
+}
+
+/***********************END OF FILE****/
