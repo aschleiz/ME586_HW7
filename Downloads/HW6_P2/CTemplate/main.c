973ee5e0f1dabee6588bdd6a7b89be20f40e3a3d
etbbb <48701990+etbbb@users.noreply.github.com>
2024-10-12 16:40:23 -0500

    Update main.c
    Update to add the problem 2 code. I like how you separated the code folders, makes it really organized!

--- Downloads/HW6_P2/CTemplate/main.c
+++ Downloads/HW6_P2/CTemplate/main.c
@@ -19,11 +19,40 @@
 /* Private functions ---------------------------------------------------------*/
 
 
+/*
+Test Report
+a. Manually sending external output without external input [PASS]
+b. 
+
+*/
+
+int UserControl = 0;
 int main(void)
 {
-		while(1){};
-} //end of main program
-
+      initcom();
+      printf("Enter a key press to start data collection\n\r");
+      initadc();
+      initdac();
+      int digVolt = 0;
+      int chan = 0;
+      int i = 0;
+    
+      while(1){
+          if (UserControl == 1){
+             digVolt = a_to_d(chan);
+             //can put some lag here.
+             printf("The USER CNTRL input voltage integer is: %d \n\r", digVolt);
+             WaitForKeypress();
+             d_to_a(chan,digVolt);
+             WaitForKeypress();}   
+          else {
+             digVolt = a_to_d(chan);
+             //can put some lag here.
+             printf("The CONT MODE input voltage integer is: %d \n\r", digVolt);
+             d_to_a(chan,digVolt);
+             }
+             }
+}
 
 void timehand(void){
 }
