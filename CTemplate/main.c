6c012e4ae668f906465d9c639592d40560c56473
etbbb <emmitsaber@gmail.com>
2024-10-17 16:34:20 -0500

    Update main.c

--- CTemplate/main.c
+++ CTemplate/main.c
@@ -21,11 +21,14 @@ struct gains {
 float kp;
 float ki;
 float kd;
-}
+};
 float setpoint;
 struct gains PIDgains;
 float controllerPeriod;
 int *ADCvalues;
+float error_prior = 0;
+float integral = 0;
+int ct = 0;
 
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
@@ -39,11 +42,14 @@ void starttime2(float millisec){
    inittime(millisec);
    //second function is to just initialize it in case the first doesn't work
 }
+
+
+
 int main(void)
 {
     int tmp;
     initcom();
-    intadc();
+    initadc();
     initdac();
 
 
@@ -52,10 +58,10 @@ int main(void)
     printf("1) set PID gains \r\n");
     printf("2) set setpoint/reference signal \r\n");
     printf("3) set controller time period \r\n");
-    printf("4) ")
+    printf("4) set storage elements \r\n");
     printf("5) set the controller loose! \r\n");
     printf("6) output X number of ADC values representing the controller output signal \r\n");
-    printf("7) exit the program");
+    printf("7) exit the program \n\r");
     USER_CHOICE = getnum();
     switch(USER_CHOICE)
     {
@@ -66,34 +72,40 @@ int main(void)
           PIDgains.ki = getfloat();
           printf("input kd \n\r");
           PIDgains.kd = getfloat();
-          break
+          break;
        case 2:
           setpoint = getfloat();
+			    break;
        case 3:
           printf("input period in millisecs \n\r");
           controllerPeriod = getfloat();
-          inittime(controllerPeriod); //this might be the best placement
+          starttime(controllerPeriod); //this might be the best placement
+			    break;
        case 4:
          printf("Input the number of elements to output \n\r");
           X = getnum();
           ADCvalues = (int *) malloc(X*sizeof(int));
+			    break;			 
        case 5:
           printf("controller is running...\n\r");
           restore_timer_interrupt();
+			    break;			 
        case 6:
           disable_timer_interrupt();
           printf("adc values: \n\r");
-          for(int = i; i<X; i++){
+          for(int i=0; i<X; i++){
               tmp = *(ADCvalues+i);
               printf("%d \n\r",tmp);
           }
+			    break;					
        case 7:
           disable_timer_interrupt();
           while(1){//done}
           }
+			    break;					
        default:
           printf("Error incorrect input \n\r");
-
+			    break;
     }
     
 
@@ -118,9 +130,8 @@ float setpoint;            <---- reference value
 struct gains PIDgains;     <---- kp,ki,kd
 float controllerPeriod;    
 int *ADCvalues;            <---- read from this
-
+   
 */
-
 
 }
 
